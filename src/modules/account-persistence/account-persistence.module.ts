a952b22eff8f8a72ff285797e372618943464f1a
Alexandru Calugari <afishr00@gmail.com>
2020-08-13 22:04:21 +0300

    Add database support

--- src/modules/account-persistence/account-persistence.module.ts
+++ src/modules/account-persistence/account-persistence.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { AccountPersistenceAdapter } from './account-persistence.adapter';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountOrmEntity } from './account.orm-entity';
+import { ActivityOrmEntity } from './activity.orm-entity';
+import { SendMoneyUseCaseSymbol } from 'src/domains/ports/in/send-money.use-case';
+import { SendMoneyService } from 'src/domains/services/send-money.service';
 
+@Global()
 @Module({
+	imports: [
+		TypeOrmModule.forFeature([AccountOrmEntity, ActivityOrmEntity])
+	],
 	providers: [
-		AccountPersistenceAdapter
+		AccountPersistenceAdapter,
+		{
+			provide: SendMoneyUseCaseSymbol,
+			useFactory: (adapter) => {
+				return new SendMoneyService(adapter, adapter)
+			},
+			inject: [AccountPersistenceAdapter]
+		}
 	],
 	exports: [
-		AccountPersistenceAdapter
+		SendMoneyUseCaseSymbol
 	]
 })
 export class AccountPersistenceModule {}
